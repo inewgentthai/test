99b03a2fc48dd2bf38e20926e2bc6227fc3f28d2
Suraches Seesat <suraches_s@becworld.com>
2022-09-14 15:57:23 +0700

    initial develop new

--- add-feature-001.js
+++ add-feature-001.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-alert('add content 001')
-alert('add content 002')
-alert('add content 003')
-
-alert('add content 004')
-=======
-Add feature new
->>>>>>> feature4
+alert('Develop original')
